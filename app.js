46ec3a4a7b185461e932c68d2bc6deda964d808d
flipott <ottphillipa@gmail.com>
2023-01-17 18:48:24 -0500

    Initial upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const mongoDb = "mongodb+srv://admin:qDV5xwNmfhzT6MEf@cluster0.yaqk0ho.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const User = mongoose.model(
+    "User",
+    new Schema({
+        username: { type: String, required: true },
+        password: { type: String, required: true }
+    })
+);
+
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+app.listen(3000, () => console.log("Currently running on Port 3000."));
