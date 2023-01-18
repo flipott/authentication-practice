56b980aa205ca788c9020a723ccc6f2631c85cac
flipott <ottphillipa@gmail.com>
2023-01-18 15:27:56 -0500

    Add views and authentication

--- app.js
+++ app.js
@@ -28,5 +28,66 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
+app.get("/", (req, res) => res.render("index", { user: req.user }));
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", (req, res, next) => {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    }).save(err => {
+      if (err) { 
+        return next(err);
+      }
+      res.redirect("/");
+    });
+});
+
+passport.use(
+    new LocalStrategy((username, password, done) => {
+        User.findOne({ username: username }, (err, user) => {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        });
+    })
+);
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+app.post("/log-in", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+}));
+
+app.get("/log-out", (req, res, next) => {
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect("/");
+    });
+});
+
+
 app.listen(3000, () => console.log("Currently running on Port 3000."));
